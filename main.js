5388d583c845424550c5079f5e26bc2a64955fdc
Tim Carrington <timiscarrington@gmail.com>
2022-11-11 09:26:47 -0800

    added input in html and javascript to name pet

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const strGameBtn= document.querySelector('#start');
 const feedPetBtn= document.querySelector('#feed');
 const playfulPetBtn = document.querySelector('#play');
 const petRestBtn = document.querySelector('#sleep');
+const form = document.querySelector("#form");
 // pet object 
 
 const pet = {
@@ -15,35 +16,32 @@ const pet = {
 //Start Game Function  will set all of the stats for pet at start of game, initialize countdown function, and a function that displays the changing health markers
 
 const startGame = () => {
-    pet.age= 0,
-    pet.hunger= 0,
-    pet.play = 0,
-    pet.sleep = 0,
+    pet.age= 1
+    pet.hunger= 1
+    pet.play = 1
+    pet.sleep = 1
     overallHealth = setInterval(wearAndTear, 1000);
     showHealth()
 }
 //function that deducts percentage from different health markers of pet
 const wearAndTear = () => {
-    pet.age += .25
-    pet.hunger += Math.floor(math.random) *.25
-    pet.play += 1
-    pet.sleep += 1
-    if(pet.sleep == 10 || pet.hunger == 10 || pet.hunger == 10){
+    pet.age += .5
+    pet.hunger += .5
+    pet.play += .5
+    pet.sleep += .5
+
+    if(pet.sleep >= 10 || pet.hunger >= 10 || pet.hunger >= 10){
         clearInterval(overallHealth)
-        alert('You killed this one. Dont try again')
-        
+        alert('You killed this one. Maybe try a house plant instead?')
     }
     showHealth();
-    // if(pet.hunger <= 0 || pet.play <= 0 || pet.sleep <= 0){
-        
-    // }
 }
 //displays the increase and decrease of pet health markers
 const showHealth = () => {
-    document.querySelector('#hunger').innerHTML = pet.hunger;
-    document.querySelector('#play').innerHTML = pet.play;
-    document.querySelector('#sleep').innerHTML = pet.sleep;
-    document.querySelector('#age').innerHTML = pet.age
+    document.querySelector('#hunger').textContent = pet.hunger;
+    document.querySelector('#play').textContent = pet.play;
+    document.querySelector('#sleep').textContent = pet.sleep;
+    document.querySelector('#age').textContent = pet.age;
 }
 
 
@@ -61,3 +59,9 @@ strGameBtn.addEventListener('click', startGame);
 feedPetBtn.addEventListener('click', feedPet);
 playfulPetBtn.addEventListener('click', playfulPet);
 petRestBtn.addEventListener('click', petRest);
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const li = document.createElement('li');
+    const input =document.querySelector('#text');
+    li.textContent=input.value;
+    document.querySelector('ul').appendChild(li);
